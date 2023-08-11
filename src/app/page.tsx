5ebb41a89bcf6edfa922f8a27dcc925d9dd79ef6
khedhar108 <khedhar.pradeep108@gmail.com>
2023-08-11 20:07:35 +0530

    Project is ready to use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
   <main>
- <h1> Now you can create your project </h1>
+ <h1 className='text-[5rem] '> Now you can create your project </h1>
   </main>
   )
 }
